a9906791961e17c5562603d7c84c8f5f97a2cbc8
Jiaxin Wang <98176596+JiaxinWang123@users.noreply.github.com>
2025-02-09 11:11:26 -0500

    Update SapFlower.m v1.0.2.1

--- SapFlower/SapFlower.m
+++ SapFlower/SapFlower.m
@@ -353,7 +353,7 @@ methods (Access = private)
             downloadUrl = parsedData.download_url;
             
             % Current version of the app
-            currentVersion = "1.0.2";  % Update this with your app's current version
+            currentVersion = "1.0.2.1";  % Update this with your app's current version
             disp("Current Version:");
             disp(currentVersion);
             disp("Latest Version:");
